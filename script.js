772819f167fdf47c550cb4d6c4adf0375ce6614d
Darcey Mckelvey <darceymckelvey@gmail.com>
2024-10-17 22:59:50 -0300

    javascript, just a bit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(document);
+
+// alert('this is an alert');
+
+console.log(document.getElementById);
+
+document.getElementsByClassName('myClass')[0].innerHTML = 'world';
+document.getElementById('myId').innerHTML = 'hello';
